b76f3692a40f50cd1ef94b0ce2ff081eb8d85ffe
Rui.Nakayama <rui19991102@gamil.com>
2026-03-12 19:40:21 +0900

    ・タイトル、内容、学習時間をフロントに送るようにした

--- backend/learning-share-api/src/main/java/io/github/ruinakayama/learning_share_api/dto/log/CreateLogResponse.java
+++ backend/learning-share-api/src/main/java/io/github/ruinakayama/learning_share_api/dto/log/CreateLogResponse.java
@@ -3,10 +3,13 @@ package io.github.ruinakayama.learning_share_api.dto.log;
 import java.time.OffsetDateTime;
 
 public record CreateLogResponse(
-        Long id,
-        String slug,
-        String shareToken,
-        Visibility visibility,
-        OffsetDateTime createdAt,
-        OffsetDateTime updatedAt) {
+                Long id,
+                String title,
+                String content,
+                Integer minutes,
+                String slug,
+                String shareToken,
+                Visibility visibility,
+                OffsetDateTime createdAt,
+                OffsetDateTime updatedAt) {
 }
