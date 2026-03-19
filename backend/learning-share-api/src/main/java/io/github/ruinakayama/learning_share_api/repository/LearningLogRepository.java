d50ada8330439c8c582b009062a1aaaba80d35e9
Rui.Nakayama <rui19991102@gamil.com>
2026-03-19 22:36:54 +0900

    ・自身のログ取得用サービスgetMyLogsの実装 ・ログ一覧取得用DTO「LogSummaryResponse」の実装 ・ログ取得用リポジトリfindByUserIdOrderByCreatedAtDescの実装

--- backend/learning-share-api/src/main/java/io/github/ruinakayama/learning_share_api/repository/LearningLogRepository.java
+++ backend/learning-share-api/src/main/java/io/github/ruinakayama/learning_share_api/repository/LearningLogRepository.java
@@ -1,5 +1,6 @@
 package io.github.ruinakayama.learning_share_api.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,4 +8,6 @@ import io.github.ruinakayama.learning_share_api.domain.LearningLogEntity;
 
 public interface LearningLogRepository extends JpaRepository<LearningLogEntity, Long> {
   Optional<LearningLogEntity> findByShareToken(String shareToken);
+
+  List<LearningLogEntity> findByUserIdOrderByCreatedAtDesc(Long userid);
 }
--- backend/learning-share-api/src/main/java/io/github/ruinakayama/learning_share_api/service/LearningLogService.java
+++ backend/learning-share-api/src/main/java/io/github/ruinakayama/learning_share_api/service/LearningLogService.java
@@ -1,16 +1,19 @@
 package io.github.ruinakayama.learning_share_api.service;
 
+import java.util.List;
+
 import io.github.ruinakayama.learning_share_api.dto.log.CreateLogRequest;
 import io.github.ruinakayama.learning_share_api.dto.log.CreateLogResponse;
-
-/*
-「LearningLogService を名乗るなら、create(userId, req) を持ってて、
-それを呼んだら CreateLogResponse を返せるようにしてね」
-っていう宣言的なイメージだよ
-*/
+import io.github.ruinakayama.learning_share_api.dto.log.LogSummaryResponse;
 
 public interface LearningLogService {
+
+  // 学習ログを新規作成する
   CreateLogResponse create(Long userId, CreateLogRequest req);
 
+  // shareToken を使って共有ログを取得する
   CreateLogResponse getByShareToken(String token);
+
+  // ログイン中ユーザー自身の学習ログ一覧を取得する
+  List<LogSummaryResponse> getMyLogs(Long userId);
 }
